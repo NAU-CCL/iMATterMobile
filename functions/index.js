014df793adea2d29f2ff72ab943716ff7ad9bce3
Victoria Leafgren <vml56@nau.edu>
2020-02-21 20:24:45 -0700

    cleaned up code

--- functions/index.js
+++ functions/index.js
@@ -9,10 +9,7 @@ require('dotenv').config()
 
 const {SENDER_EMAIL, SENDER_PASSWORD}= process.env;
 
-exports.sendEmailNotification=functions.https.onRequest((req, res)=>{
-	//var ref = admin.firestore().doc("providers/{docID}");
-	//const data=ref.data();
-	
+exports.sendEmailNotification=functions.https.onRequest((req, res)=>{	
 	let authData = nodemailer.createTransport({
 		host:'smtp.gmail.com',
 		port:587,
@@ -24,111 +21,34 @@ exports.sendEmailNotification=functions.https.onRequest((req, res)=>{
 		
 		
 });
-/*
-ref = admin.firestore().collection('/providers/');
-ref.where('code', '==', 3oYi4BQiDYOY6vDfpZsp )
-            .get().then(snap => {
-				var email = ref.email;
-				
-          /*if (snap.docs.length > 0) {
-            const userRef = ref.where('code', '==', this.userProfileID);
-            userRef.get().then((result) => {
-              result.forEach(doc => {
-                this.userType = 'provider';
-                this.provider.username = doc.get('username');
-                this.provider.bio = doc.get('bio');
-                this.provider.profilePic = doc.get('profilePic');
-              });
-            });
-          }
-        }); */
 
-//admin.firestore().doc('/providers/').once('value').then(function(snapshot) {
-//var email = snapshot.val().email;
-
-//});
-var maillist = [
-  'vml56@nau.edu',
-  'toryleafgren@gmail.com',
-];
-
-/*var stuff = [];
-var db = admin.firestore();
-    db.collection("providers").get().then(snapshot => {
-console.log("1");
-        snapshot.forEach(doc => {
-			console.log("2");
-            var newelement = {
-				
-				"email": doc.data().email
-				
-            }
-			console.log("5");
-			console.log(email);
-			res.send(email);
-			console.log(email);
-            stuff = stuff.concat(newelement);
-        });
-        //res.send(stuff)
-		console.log("6");
-		console.log(stuff);
-        return "";
-    }).catch(reason => {
-		console.log(":/");
-        res.send(reason)
-    })*/
-
-//console.log("1");
-const ref = admin.firestore().collection('providers');
-        ref.get().then((result) => {
-			//console.log("2")
-          result.forEach(doc => {
-            email = doc.get('email');
-			//console.log("4");
-            console.log(email);
-			authData.sendMail({
-				//console.log(ref.email);
-				from: 'imatternotification@gmail.com',
-				to: email, // list of receivers
-				subject: "Imatter InfoDesk", // Subject line
-				text: "There is a new question on the InfoDesk!", // plain text body
-				html: "<b>There is a new question on the InfoDesk!</b>" // html body
-			}).then(res=>console.log('successfully sent that mail')).catch(err=>console.log(err));
-	      });
-		  //console.log("5");
-		  res.send("Emails have been sent");
-		  return null;
-		}).catch(reason => {
-		
-        res.send(email)
-    })
+	const ref = admin.firestore().collection('providers');
+			ref.get().then((result) => {			
+			  result.forEach(doc => {
+				email = doc.get('email');
+				console.log(email);
+				authData.sendMail({
+					from: 'imatternotification@gmail.com',
+					to: email, // list of receivers
+					subject: "Imatter InfoDesk", // Subject line
+					text: "There is a new question on the InfoDesk!", // plain text body
+					html: "<b>There is a new question on the InfoDesk!</b>" // html body
+				}).then(res=>console.log('successfully sent that mail')).catch(err=>console.log(err));
+			  });
+			  //if the res.send is the same each time, for some reason it stops working? Added random number so its different each send.
+			  var number = Math.random();
+			  res.send("Emails have been sent" + number);
+			  return null;
+			}).catch(reason => {
+			
+			res.send(email)
+		})
 
 
 });
 
 
-/*
-function sendEmail(){
-	let transporter = nodemailer.createTransport({
-		host: "smtp.imatternotification@gmail.com",
-		port: 587,
-		secure: false, // true for 465, false for other ports
-		auth: {
-		  user: iMatter Notification, 
-		  pass: WordPass1 
-		}
-	  });
 
-	  // send mail with defined transport object
-	  let info = await transporter.sendMail({
-		from: '"Fred Foo 👻" <foo@example.com>', // sender address
-		to: "bar@example.com, baz@example.com", // list of receivers
-		subject: "Hello ✔", // Subject line
-		text: "Hello world?", // plain text body
-		html: "<b>Hello world?</b>" // html body
-	  });
-}
-*/
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
