439b719a87c08cb8a5530682d0ad851a4c6fd44d
Jennie <jad554@nau.edu>
2020-02-24 00:49:24 -0700

    firebase functions stuff checkpoint

---
+++ functions/index.js
@@ -0,0 +1,85 @@
+const functions = require('firebase-functions');
+const admin=require('firebase-admin');
+const nodemailer = require('nodemailer');
+admin.initializeApp(functions.config().firebase);
+
+
+//admin.initializeApp();
+require('dotenv').config()
+
+const {SENDER_EMAIL, SENDER_PASSWORD}= process.env;
+
+exports.sendEmailNotification=functions.https.onRequest((req, res)=>{	
+	let authData = nodemailer.createTransport({
+		host:'smtp.gmail.com',
+		port:587,
+		secure: false,
+		auth: {
+		  user: SENDER_EMAIL, 
+		  pass: SENDER_PASSWORD 
+		}
+		
+		
+});
+
+	const ref = admin.firestore().collection('providers');
+			ref.get().then((result) => {			
+			  result.forEach(doc => {
+				email = doc.get('email');
+				console.log(email);
+				authData.sendMail({
+					from: 'imatternotification@gmail.com',
+					to: email, // list of receivers
+					subject: "Imatter InfoDesk", // Subject line
+					text: "There is a new question on the InfoDesk!", // plain text body
+					html: "<b>There is a new question on the InfoDesk!</b>" // html body
+				}).then(res=>console.log('successfully sent that mail')).catch(err=>console.log(err));
+			  });
+			  //if the res.send is the same each time, for some reason it stops working? Added random number so its different each send.
+			  var number = Math.random();
+			  res.send("Emails have been sent" + number);
+			  return null;
+			}).catch(reason => {
+			
+			res.send(email)
+		})
+
+
+});
+
+exports.sendRecoveryEmail=functions.firestore.document('recovery_email/{docID}').onCreate((snap,context)=>{	
+	const data=snap.data();
+	let authData = nodemailer.createTransport({
+		host:'smtp.gmail.com',
+		port:587,
+		secure: false,
+		auth: {
+		  user: SENDER_EMAIL, 
+		  pass: SENDER_PASSWORD 
+		}
+		
+		
+});
+
+	authData.sendMail({
+		from: 'imatternotification@gmail.com',
+		to: data.email, // list of receivers
+		subject: "Imatter InfoDesk", // Subject line
+		text: "Here is your recovery code: " + data.code, // plain text body
+		html: "Here is your recovery code: " + data.code // html body
+		//res.send("sent");
+		}).then(res=>console.log('successfully sent that mail')).catch(err=>console.log(err));
+	});
+
+
+//https://firebase.google.com/docs/firestore/extend-with-functions
+exports.newLearningModuleNotification = functions.firestore.document('')
+
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
