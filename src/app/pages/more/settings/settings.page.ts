128b1dd27daa476d8950ff187e128fedd58d808d
biancaaltman <43017874+biancaaltman@users.noreply.github.com>
2020-02-24 12:54:19 -0700

    refactor toggle

--- src/app/pages/more/settings/settings.page.ts
+++ src/app/pages/more/settings/settings.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Storage } from '@ionic/storage';
+import { FcmService } from "../../../services/pushNotifications/fcm.service";
 
 @Component({
   selector: 'app-settings',
@@ -11,12 +12,25 @@ export class SettingsPage implements OnInit {
 
   private chatNotif: boolean;
 
-  constructor(public afs: AngularFirestore, private storage: Storage,) {
-    this.chatNotif = true;
+  constructor(public afs: AngularFirestore, private storage: Storage, private fcm: FcmService) {
+
   }
 
   ngOnInit() {
-
+    this.storage.get('userCode').then((val) => {
+      if (val) {
+        this.afs.firestore.collection('users').where('code', '==', val)
+            .get().then(snapshot => {
+          snapshot.forEach(doc => {
+            if (doc.get('chatNotif') === false) {
+              this.chatNotif = false;
+            } else {
+              this.chatNotif = true;
+            }
+          });
+        });
+      }
+    });
   }
 
 
@@ -32,10 +46,16 @@ export class SettingsPage implements OnInit {
                 } else {
                   this.afs.firestore.collection('users')
                       .doc(val).update({chatNotif: true});
+                  this.notificationSetup(val);
                 }
               });
             });
       }
     });
+  }
+
+  private notificationSetup(userID) {
+    console.log(userID);
+    this.fcm.getToken(userID);
   }
 }
