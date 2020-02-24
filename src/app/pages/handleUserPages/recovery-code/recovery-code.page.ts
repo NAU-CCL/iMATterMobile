ae1747a85301ca3a86ddd59544daf876177e8475
Victoria Leafgren <vml56@nau.edu>
2020-02-23 21:45:08 -0700

    almost working

--- src/app/pages/handleUserPages/recovery-code/recovery-code.page.ts
+++ src/app/pages/handleUserPages/recovery-code/recovery-code.page.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import {ToastController} from '@ionic/angular';
 import { Storage } from '@ionic/storage';
+import { ProfileService } from '../../../services/user/profile.service';
 
 @Component({
   selector: 'app-recovery-code',
@@ -23,6 +24,9 @@ export class RecoveryCodePage implements OnInit {
     private userPassword: string;
 	private recoveryCode: string;
 	private theCode: string;
+	private wantedUserID: string;
+	private recoveryPassword: string;
+	
 
   public enterCodeForm: FormGroup;
   constructor(
@@ -33,10 +37,15 @@ export class RecoveryCodePage implements OnInit {
         private formBuilder: FormBuilder,
         public afs: AngularFirestore,
         private toastCtrl: ToastController,
-        private storage: Storage
+        private storage: Storage,
+		private profileService: ProfileService
   ) {
     this.enterCodeForm = this.formBuilder.group({
       recoveryCode: [
+        '',
+        Validators.compose([Validators.required]),
+      ],
+	  recoveryPassword: [
         '',
         Validators.compose([Validators.required]),
       ],
@@ -50,10 +59,13 @@ export class RecoveryCodePage implements OnInit {
   
   validateUser(enterCodeForm: FormGroup) {
         this.recoveryCode = enterCodeForm.value.recoveryCode;
+		this.recoveryPassword = enterCodeForm.value.recoveryPassword;
         var recoveryEmail;
 		var theCode;
 		console.log("1");
 		console.log(this.recoveryCode);
+		console.log("recoveryEmail");
+		console.log(this.recoveryPassword);
         this.afs.firestore.collection('recovery_email').where('code', '==', this.recoveryCode)
             .get().then(snapshot => {
 				console.log("2");
@@ -88,6 +100,7 @@ export class RecoveryCodePage implements OnInit {
                     result.forEach(doc => {
                         this.userID = doc.id;
                         this.userEmail = doc.get('email');
+						this.password = doc.get('password');
 
                         if ( this.userEmail === recoveryEmail) {
                             //this.storage.set('userCode', this.userID);
@@ -96,16 +109,27 @@ export class RecoveryCodePage implements OnInit {
                             //this.storage.set('dueDate', doc.get('dueDate'));
                             //this.storage.set('cohort', doc.get('cohort'));
 							console.log("Ya!");
-                            
-
-
-                            //this.router.navigate(['/tabs/home/']);
+							//updatePassword();
+							//this.doc.password = "wordpass";
+                            //this.storage.set('password', 'wordpass');
+							//doc.password = 'wordpass';
+							//console.log(doc.get('password'));
+							//doc.update({
+							//	password: 'wordpass'
+							//});
+							console.log(this.recoveryPassword);
+							this.wantedUserID = this.userID;
+							this.afs.firestore.collection('users').doc(this.wantedUserID).update({
+								password: this.recoveryPassword
+							});
+							//this.router.navigate(['/tabs/home/']);
                         } else {
                             //this.showToast('Password is incorrect');
                         }
 
                     });
                 });
+				
 
             } else {
                 console.log('Email does not exist');
@@ -113,5 +137,27 @@ export class RecoveryCodePage implements OnInit {
             }
         });
     }
+	async updatePassword(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      inputs: [
+        { name: 'newPassword', placeholder: 'New password', type: 'password' },
+        { name: 'oldPassword', placeholder: 'Old password', type: 'password' },
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileService.updatePassword(
+                data.newPassword,
+                data.oldPassword, this.userID
+            );
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+	
 
 }
