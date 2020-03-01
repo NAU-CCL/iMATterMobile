10574a622c127eac3607e14d0a72c9ade6e073c8
cv456 <cv456@nau.edu>
2020-02-29 21:12:03 -0700

    custom pin to Canes
    added a custom pin to Canes !<3

--- src/app/pages/more/resources/resources.module.ts
+++ src/app/pages/more/resources/resources.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { ResourcesPageRoutingModule } from './resources-routing.module';
 
 import { ResourcesPage } from './resources.page';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { ResourcesPage } from './resources.page';
     IonicModule,
     ResourcesPageRoutingModule
   ],
-  declarations: [ResourcesPage]
+  declarations: [ResourcesPage],
+  providers: [Geolocation]
 })
 export class ResourcesPageModule {}
--- src/app/pages/more/resources/resources.page.ts
+++ src/app/pages/more/resources/resources.page.ts
@@ -20,25 +20,60 @@ export class ResourcesPage implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit(): void {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.latitude = resp.coords.latitude;
-      this.longitude = resp.coords.longitude;
-      const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 6
-    });
-    const infoWindow = new google.maps.InfoWindow;
-    const pos = {
-      lat: this.latitude,
-      lng: this.longitude
-    };
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    infoWindow.open(map);
-    map.setCenter(pos);
-  }).catch((error) => {
-    console.log('Error getting location', error);
-  });
-}
+      this.geolocation.getCurrentPosition().then((resp) => {
+         this.latitude = resp.coords.latitude;
+         this.longitude = resp.coords.longitude;
+         const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
+           center: {lat: 35.185261, lng: -111.662041},
+           zoom: 16
+         });
+         /*location object*/
+         const pos = {
+           lat: this.latitude,
+           lng: this.longitude
+         };
+         map.setCenter(pos);
+         const icon = {
+           url: 'https://firebasestorage.googleapis.com/v0/b/techdemofirebase.appspot.com/o/locationIcon%2Flocationpin.png?alt=media&token=a04dd171-e687-4504-a9ae-53eb1cb3986f', // image url
+           scaledSize: new google.maps.Size(80, 80), // scaled size
+         };
+         const marker = new google.maps.Marker({
+           position: pos,
+           map: map,
+           title: 'Clinc',
+           icon: icon
+         });
+         const contentString = '<div id="content">' +
+             '<div id="siteNotice">' +
+             '</div>' +
+             '<h1 id="firstHeading" class="firstHeading">I AM HERE .................</h1>' +
+             '<div id="bodyContent">' +
+             '<img src="assets/icon/user.png" width="200">' +
+             '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+             'sandstone rock formation in the southern part of the ' +
+             'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+             'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+             '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+             'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+             'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+             'Aboriginal people of the area. It has many springs, waterholes, ' +
+             'rock caves and ancient paintings. Uluru is listed as a World ' +
+             'Heritage Site.</p>' +
+             '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+             'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+             '(last visited June 22, 2009).</p>' +
+             '</div>' +
+             '</div>';
+         const infowindow = new google.maps.InfoWindow({
+           content: contentString,
+           maxWidth: 400
+         });
+         marker.addListener('click', function() {
+           infowindow.open(map, marker);
+         });
+       }).catch((error) => {
+         console.log('Error getting location', error);
+       });
+     }
 
   }
