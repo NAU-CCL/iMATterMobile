069de3eb1e6751418875a49036ec2948a41aac55
Victoria Leafgren <vml56@nau.edu>
2020-03-01 20:43:27 -0700

    creating function

--- functions/index.js
+++ functions/index.js
@@ -73,7 +73,43 @@ exports.sendRecoveryEmail=functions.firestore.document('recovery_email/{docID}')
 
 
 
+exports.updateDays=functions.https.onRequest((req, res)=>{	
+	let authData = nodemailer.createTransport({
+		host:'smtp.gmail.com',
+		port:587,
+		secure: false,
+		auth: {
+		  user: SENDER_EMAIL, 
+		  pass: SENDER_PASS 
+		}
+		
+		
+});
 
+	const ref = admin.firestore().collection('providers');
+			ref.get().then((result) => {			
+			  result.forEach(doc => {
+				email = doc.get('email');
+				console.log(email);
+				authData.sendMail({
+					from: 'imatternotification@gmail.com',
+					to: email, // list of receivers
+					subject: "Imatter InfoDesk", // Subject line
+					text: "There is a new question on the InfoDesk!", // plain text body
+					html: "<b>There is a new question on the InfoDesk!</b>" // html body
+				}).then(res=>console.log('successfully sent that mail')).catch(err=>console.log(err));
+			  });
+			  //if the res.send is the same each time, for some reason it stops working? Added random number so its different each send.
+			  var number = Math.random();
+			  res.send("Emails have been sent" + number);
+			  return null;
+			}).catch(reason => {
+			
+			res.send(email)
+		})
+
+
+});
 
 
 
