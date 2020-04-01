df6fa8b5b1ffbaf3adb3cc63da1f0e8d170fb6ec
Victoria Leafgren <vml56@nau.edu>
2020-03-31 21:12:13 -0700

    Attempting change firebase notification times

--- src/app/pages/more/settings/settings.page.ts
+++ src/app/pages/more/settings/settings.page.ts
@@ -143,5 +143,20 @@ export class SettingsPage implements OnInit {
       }
     });
   }
+  setCalendarEventNotificationTime(eventTime){
+	  this.storage.get('userCode').then((val) => {
+      if (val) {
+        this.afs.firestore.collection('users').where('code', '==', val)
+            .get().then(snapshot => {
+          snapshot.forEach(doc => {
+              this.afs.firestore.collection('users')
+                  .doc(val).update({notificationTime: eventTime.value});
+            
+            
+          });
+        });
+      }
+    });
+}
 
 }
