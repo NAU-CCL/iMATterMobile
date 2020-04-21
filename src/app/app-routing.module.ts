6c5354a3c92c88ef7506c40855ba3a8a84688810
biancaaltman <43017874+biancaaltman@users.noreply.github.com>
2020-04-21 12:27:28 -0700

    attempts at fixing unit tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './services/user/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
