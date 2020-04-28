717603f0a527bbac2a365ab8153860395c2213b8
VictoriaLeafgren <47437111+VictoriaLeafgren@users.noreply.github.com>
2020-04-27 17:59:07 -0700

    fixing calendar settings page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -81,7 +81,15 @@ const routes: Routes = [
   {
     path: 'recovery-code',
     loadChildren: () => import('./pages/handleUserPages/recovery-code/recovery-code.module').then( m => m.RecoveryCodePageModule)
+  },  {
+    path: 'calendar-settings',
+    loadChildren: () => import('./calendar-settings/calendar-settings.module').then( m => m.CalendarSettingsPageModule)
+  },
+  {
+    path: 'calendar-settings',
+    loadChildren: () => import('./pages/calendar-settings/calendar-settings.module').then( m => m.CalendarSettingsPageModule)
   }
+
 ];
 @NgModule({
   imports: [
