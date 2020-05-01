f624bd600063428434041ed86dc45b4abe2be213
cv456 <cv456@nau.edu>
2020-05-01 15:52:28 -0700

    updating bug and log out procedures for timeout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { AnalyticsService, Analytics, Sessions  } from 'src/app/services/analyticsService.service';
 import * as firebase from 'firebase/app';
 import { firebaseConfig } from './firebaseCredentials';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 import { Storage } from '@ionic/storage';
 import { BnNgIdleService } from 'bn-ng-idle';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -17,23 +19,42 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class AppComponent {
+  session: Sessions =
+      {
+          userID: '',
+          LogOutTime: '',
+          LoginTime: '',
+          numOfClickChat: 0,
+          numOfClickCalendar: 0,
+          numOfClickLModule: 0,
+          numOfClickInfo: 0,
+          numOfClickSurvey: 0,
+          numOfClickProfile: 0,
+          numOfClickMore: 0,
+          numOfClickHome: 0
+      };
+  public sessions: Observable<any>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private storage: Storage,
     private router: Router,
-    private bnIdle: BnNgIdleService
+    private bnIdle: BnNgIdleService,
+    private afs: AngularFirestore,
+    private analyticsService: AnalyticsService
   ) {
+
+
+
     this.initializeApp();
     firebase.initializeApp(firebaseConfig);
     this.bnIdle.startWatching(300).subscribe((isTimedOut: boolean) => {
        if (isTimedOut) {
-
+         this.updateLogOut();
          console.log('session expired');
-
          this.logOut();
-
        }
      });
   }
@@ -45,6 +66,12 @@ export class AppComponent {
     this.storage.remove('weeksPregnant');
     this.storage.remove('daysPregnant');
     this.router.navigateByUrl('login');
+  }
+
+
+  updateLogOut() {
+   this.analyticsService.updateLogOut(this.session);
+   console.log('added LogOutTime');
   }
 
   initializeApp() {
