f5d5ac5252a2dcbd629bb152c1f04eebf12371eb
cv456 <cv456@nau.edu>
2020-05-04 23:05:47 -0700

    YAY MAPS <3

--- src/app/pages/more/resources/resources-routing.module.ts
+++ src/app/pages/more/resources/resources-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 
 import { ResourcesPage } from './resources.page';
 
