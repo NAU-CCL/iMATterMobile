7d5ea49c286683c070282d39d53067c24780b270
Calvin Harper <cch264@nau.edu>
2022-03-11 17:08:25 -0700

    Resource reviews now loaded on demand

--- src/app/pages/more/resources/display-reviews/display-reviews.page.ts
+++ src/app/pages/more/resources/display-reviews/display-reviews.page.ts
@@ -19,6 +19,7 @@ export class DisplayReviewsPage implements OnInit {
   public starArray = [1,2,3,4,5];
 
   public reviewRefArray = [];
+  public reviewDocArray = [];
   public currentReviewRefIndex = -1;
 
   @Input('resource_name') resourceTitle; 
@@ -49,73 +50,38 @@ export class DisplayReviewsPage implements OnInit {
   {
     this.currentReviewRefIndex += 1;
 
-    console.log(`In review load func, about to wait for promise.`)
+    //console.log(`In review load func, about to wait for promise.`)
 
     return this.reviewRefArray[this.currentReviewRefIndex].get().then( ( docSnap ) => { 
       let docData = docSnap.data();
-      console.log(`DOC SNAP BEFORE RETURN ${JSON.stringify(docData) }`)
+      //console.log(`DOC SNAP BEFORE RETURN ${JSON.stringify(docData) }`)
       return docData  });
   }
 
 
   loadData(event) {
     setTimeout( async () => {
-      let listElem: HTMLElement = document.getElementById('review-list');
-
-      console.log(`Before getting review:`);
 
       let currentReview = await this.loadReviewForIndex();
 
-      console.log(`After getting review:`);
-
-      let cardContainerElem = document.createElement('div');
-
-      cardContainerElem.innerHTML = this.generateReviewCardElem( currentReview );
+      this.appendReviewToArray( currentReview );
 
       event.target.complete();
 
-      listElem.append( cardContainerElem );
-
       // App logic to determine if all data is loaded
       // and disable the infinite scroll
-      if (this.currentReviewRefIndex >= this.reviewRefArray.length ) {
+      if (this.currentReviewRefIndex >= this.reviewRefArray.length - 1 ) {
         event.target.disabled = true;
+        console.log(`Disabling spinner`);
       }
     }, 100);
   }
 
 
-  generateReviewCardElem( review : Review)
+  // Appends the review object to the array of reviews for display.
+  appendReviewToArray( review : Review)
   {
-    console.log(`GENERATING REVIEW: ${JSON.stringify(review)}`);
-    return `<ion-card>
-    <ion-card-header class="ion-no-padding review-card-header-pad">
-
-      <ion-card-title> 
-        ${review.reviewSubject}
-      </ion-card-title>
-
-      <ion-card-subtitle>
-        <div id="review-card-star-container" class="dis-flex" >
-          <ion-icon *ngFor="let num of starArray" id="star-1" [attr.name]="num <= review.reviewRating ? 'star' : 'star-outline'" size="small"></ion-icon>
-          <span>  -  ${review.reviewDate}</span>
-        </div> 
-
-        
-
-       </ion-card-subtitle>
-
-    </ion-card-header>
-  
-    <ion-card-content>
-
-    
-
-
-      <span> ${review.reviewText}</span>
-
-    </ion-card-content>
-  </ion-card>`
+    this.reviewDocArray.push(review);
   }
   
 }
