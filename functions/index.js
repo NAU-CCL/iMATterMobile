3600fe9a269c6ce9fa2ad94b8a8fe6f55bd80489
koseward <kos34@nau.edu>
2022-08-22 15:33:11 -0700

    add the ability to export csvs from a file

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,72 @@ var newChat;
 const nodemailer = require('nodemailer');
 admin.initializeApp(functions.config().firebase);
 
+import * as functions from "firebase-functions";
+import * as firebase from "firebase-admin";
+import { parseAsync } from 'json2csv';
+import { v4 as uuidv4 } from 'uuid';
+import * as fs from "fs";
+import * as path from "path";
+import * as os from "os";
+
+firebase.initializeApp({
+  storageBucket: 'storage-bucket-name',
+});
+
+export const generateApplicationCsv = functions.region('asia-northeast1').pubsub
+  .topic("generate-application-csv")
+  .onPublish(async message => {
+
+    // gets the documents from the firestore collection
+    const applicationsSnapshot = await firebase
+      .firestore()
+      .collection("applications")
+      .get();
+
+    const applications = applicationsSnapshot.docs.map(doc => doc.data());
+
+    // csv field headers
+    const fields = [
+      'firstName',
+      'lastName',
+    ];
+
+    // get csv output
+    const output = await parseAsync(applications, { fields });
+
+    // generate filename
+    const dateTime = new Date().toISOString().replace(/\W/g, "");
+    const filename = `applications_${dateTime}.csv`;
+
+    const tempLocalFile = path.join(os.tmpdir(), filename);
+
+    return new Promise((resolve, reject) => {
+      //write contents of csv into the temp file
+      fs.writeFile(tempLocalFile, output, error => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        const bucket = firebase.storage().bucket();
+
+        // upload the file into the current firebase project default bucket
+        bucket
+           .upload(tempLocalFile, {
+            // Workaround: firebase console not generating token for files
+            // uploaded via Firebase Admin SDK
+            // https://github.com/firebase/firebase-admin-node/issues/694
+            metadata: {
+              metadata: {
+                firebaseStorageDownloadTokens: uuidv4(),
+              }
+            },
+          })
+          .then(() => resolve())
+          .catch(errorr => reject(errorr));
+      });
+    });
+  });
+
 // Tried uncommenting this line to get firebase push notif authentication error to go away.
 //admin.initializeApp();
 require('dotenv').config();
