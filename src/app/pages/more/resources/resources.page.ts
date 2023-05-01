8b240455a1aaa3f47021f5ac19311b852695f6c3
koseward <kos34@nau.edu>
2023-05-01 13:01:55 -0700

    Feat: removed unecessary buttons from the resources map. Feat: added a button to recenter the map to the resources map

--- src/app/pages/more/resources/resources.page.ts
+++ src/app/pages/more/resources/resources.page.ts
@@ -174,17 +174,64 @@ export class ResourcesPage implements OnInit, AfterViewInit {
         await this.geolocation.getCurrentPosition().then((resp) => {
             this.latitude = resp.coords.latitude;
             this.longitude = resp.coords.longitude;
-            console.log(this.latitude);
-            console.log(this.longitude);
             this.map = new google.maps.Map(this.mapNativeElement.nativeElement, {
                 center: { lat: this.latitude, lng: this.longitude },
-                zoom: 16
+                zoom: 16,
+                mapTypeControl: false,
+                fullscreenControl: false,
+                streetViewControl: false
             });
+
+        // Add button to recenter the map to the starting position
+          // Create the DIV to hold the control.
+        const centerControlDiv = document.createElement('div');
+        // Create the control.
+        const centerControl = this.createCenterControl(this.map);
+        // Append the control to the DIV.
+        centerControlDiv.appendChild(centerControl);
+
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(centerControlDiv);
+
 
         }).catch((error) => {
             console.log('ERROR LOADING MAP ', error);
         });
     }
+
+    /**
+ * Creates a control that recenters the map on The user's starting location.
+ */
+ createCenterControl(map) {
+    const controlButton = document.createElement('button');
+  
+    // Set CSS for the control.
+    controlButton.style.backgroundColor = '#fff';
+    controlButton.style.border = '2px solid #fff';
+    controlButton.style.borderRadius = '3px';
+    controlButton.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlButton.style.color = 'rgb(25,25,25)';
+    controlButton.style.cursor = 'pointer';
+    controlButton.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlButton.style.fontSize = '16px';
+    controlButton.style.lineHeight = '38px';
+    controlButton.style.margin = '8px 0 22px';
+    controlButton.style.padding = '0 5px';
+    controlButton.style.textAlign = 'center';
+  
+    controlButton.textContent = 'Center Map';
+    controlButton.title = 'Click to recenter the map';
+    controlButton.type = 'button';
+  
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlButton.addEventListener('click', () => {
+        const startingLocation = { lat: this.latitude, lng: this.longitude }
+        map.setCenter(startingLocation);
+        map.setZoom( 16 );
+    });
+  
+    return controlButton;
+  }
+  
 
 
     async addMarker(location) {
